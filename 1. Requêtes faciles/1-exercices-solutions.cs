dc1959ea6b1503ecd6947fae26ee111936087653
Foxx py <68949615+foxxpy@users.noreply.github.com>
2021-03-31 19:13:04 +0200

    Ajout des scripts de l'épisode 1 et 2
    Ajout des scripts contenant les énoncés et les solutions des exercices des épisodes 1 et 2 sur Linq.

---
+++ 1. Requêtes faciles/1-exercices-solutions.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqFoxxpy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> entiers = new List<int> {4,5,2,3,1,1,0,5,8,9,10,15,16,20,21,4,5 };
+
+            //1. Récupérer tous les nombres supérieurs à 5
+            var query = from n in entiers
+                        where n > 5
+                        select n;
+
+            //2. Récupérer les nombres supérieurs ou égaux à 15 et inférieurs à 20
+            var query2 = from n in entiers
+                         where n >= 15 && n < 20
+                         select n;
+
+            var query2_2 = from n in entiers
+                           where n >= 15
+                           where n < 20
+                           select n;
+
+            //3. Nombres supérieurs à 2, qui sont des multiples de 2, inférieurs à 20, différents de 8
+            var query3 = from n in entiers
+                        where n > 2
+                        where n % 2 == 0
+                        where n < 20
+                        where n != 8
+                        select n;
+
+            var query3_2 = from n in entiers
+                           where n > 2 && n % 2 == 0 && n < 20 && n != 8
+                           select n;
+
+            List<string> fruits = new List<string> { "Banane", "Ananas", "Cerise", "Framboise", "Groseilles", "Pomme",
+            "Poire", "Tomate", "Kiwi", "Raisin", "Mangue", "Datte"};
+
+            //4. Récupérer tous les fruits dont le nom contient plus de 5 lettres
+            var query4 = from f in fruits
+                         where f.Length > 5
+                         select f;
+
+            //5. Récupérer tous les fruits dont le nom commence par "P", dont la longueur du nom est supérieure à 4, 
+            //qui contiennent un "o" mais pas de "m"
+            var query5 = from f in fruits
+                         where f.ToUpper()[0] == 'P'
+                         where f.Length > 4
+                         where f.Contains('o')
+                         where !f.Contains('m')
+                         select f;
+
+
+            //6. Trier les fruits selon leur longueur
+            var query6 = from f in fruits
+                         orderby f.Length
+                         select f;
+
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+
+}
---
+++ 2. Requêtes sur les objets/2-exercices-et-solutions.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqFoxxpy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dog> dogs = new List<Dog>
+            {
+                new Dog("Berger Australien", "Banzaï", 1, 28),
+                new Dog("Berger Australien", "Letto", 3, 30),
+                new Dog("Berger Australien", "Princesse", 8, 32),
+                new Dog("Berger Allemand", "Floyd", 10, 32),
+                new Dog("Caniche", "Igor", 13, 9),
+                new Dog("Labrador", "Swing", 15, 25),
+                new Dog("Teckel", "Wonki", 2, 5),
+                new Dog("Terre Neuve", "Albator", 1, 50),
+                new Dog("Carlin", "Pataud", 13, 10),
+                new Dog("Boxer", "Frank", 6, 25),
+                new Dog("Lévrier Afghan", "Précieuse", 9, 26),
+                new Dog("Yorkshire", "Kakou", 3, 6)
+            };
+            //1. Récupérer tous les chiens qui sont de la race "Berger Australien"
+            var query = from d in dogs
+                        where d.Race == "Berger Australien"
+                        select d;
+
+            //1.5. Récupérer tous les chiens qui sont de la race "Berger Australien" et les trier par leur nom
+            var query2 = from d in dogs
+                        where d.Race == "Berger Australien"
+                        orderby d.Name
+                        select d;
+
+            //2. Récupérer tous les chiens âgés de 5 ans et plus, dont la longueur du nom est supérieure à 5 lettres
+            var query3 = from d in dogs
+                         where d.Age >= 5 && d.Name.Length > 5
+                         select d;
+
+            //2.5 Récupérer tous les chiens âgés de 5 ans et plus, dont la longueur du nom est supérieure à 5 lettres
+            //Trier les chiens par leur poids
+            var query4 = from d in dogs
+                         where d.Age >= 5 && d.Name.Length > 5
+                         orderby d.Weight ascending
+                         select d;
+
+            //3. Trier les chiens par leur âge (tri décroissant) puis leur poids (tri croissant)
+            var query5 = from d in dogs
+                         orderby d.Age descending, d.Weight ascending
+                         select d;
+
+            //4. Récupérer les noms de chien dont le nom de race tient en un seul mot
+            //Leur poids doit être supérieur à 15 kilos
+            //Leur nom doit contenir un "i"
+            //Trier les chiens par la longueur de leur prénom
+            var query6 = from d in dogs
+                        where !d.Race.Contains(" ")
+                        where d.Weight > 15
+                        where d.Name.ToLower().Contains("i")
+                        orderby d.Name.Length ascending
+                        select d;
+
+            foreach (var item in query)
+            {
+                Console.WriteLine(item.Name);
+            }
+
+        }
+    }
+
+    internal class Dog
+    {
+        private string race;
+        private string name;
+        private int age;
+        private int weight;
+        
+        public Dog(string race, string name, int age, int weight)
+        {
+            this.race = race;
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+        }
+
+        public string Race
+        {
+            get
+            {
+                return this.race;
+            }
+            set
+            {
+                this.race = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                this.age = value;
+            }
+        }
+
+        public int Weight
+        {
+            get
+            {
+                return this.weight;
+            }
+            set
+            {
+                this.weight = value;
+            }
+        }
+
+    }
+}
