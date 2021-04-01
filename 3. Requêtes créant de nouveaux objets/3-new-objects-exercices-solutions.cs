45177040c9d8bc6e103a8ca1650b5058f1332b54
Foxx py <68949615+foxxpy@users.noreply.github.com>
2021-04-01 17:08:07 +0200

    Ajout des scripts épisode 3 et 4

---
+++ 3. Requêtes créant de nouveaux objets/3-new-objects-exercices-solutions.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqFoxxpy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Personne> personnes = new List<Personne>
+            {
+                new Personne("Hallyday", "Johnny", false),
+                new Personne("Vartan", "Sylvie", false),
+                new Personne("Drucker", "Michel", false),
+                new Personne("Antoine", "Antoine", true),
+                new Personne("Philippe", "Edouard", false),
+                new Personne("Demorand", "Patricia", true),
+                new Personne("Ulysse", "Margareth", true),
+                new Personne("Zenith", "Méryl", true),
+                new Personne("Bobo", "Jojo", false)
+            };
+
+            //1. Créer un itérable d'ingénieurs, trier par nom, et ensuite par prénom
+            var queryIngenieurs = from p in personnes
+                        where p.Est_ingenieur
+                        orderby p.Nom, p.Prenom
+                        select new Ingenieur(p.Nom, p.Prenom);
+
+
+            //2. Récupérer la liste des personnes qui ne sont pas ingénieures.
+            var queryTechniciens = from p in personnes
+                                   where !p.Est_ingenieur
+                                   select new Technicien(p.Nom, p.Prenom);
+
+
+            //3. Créer une liste d'objets anonymes (Ingénieurs + techniciens)
+            var queryAnonymous = from p in personnes
+                                 select new {nom_complet = p.Nom+" "+p.Prenom};
+
+            
+            foreach (var item in queryAnonymous)
+            {
+                Console.WriteLine(item.nom_complet);
+            }
+
+        }
+
+    }
+
+    class Personne
+    {
+        private string nom;
+        private string prenom;
+        private bool est_ingenieur;
+
+        public Personne(string nom, string prenom, bool ingenieur)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.est_ingenieur = ingenieur;
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return this.nom;
+            }
+            set
+            {
+                this.nom = value;
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return this.prenom;
+            }
+
+            set
+            {
+                this.prenom = value;
+            }
+        }
+
+        public bool Est_ingenieur
+        {
+            get
+            {
+                return this.est_ingenieur;
+            }
+            set
+            {
+                this.est_ingenieur = value;
+            }
+        }
+
+    }
+
+    class Ingenieur
+    {
+        private string nom;
+        private string prenom;
+        private string specialite;
+
+        public Ingenieur(string nom, string prenom)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+        }
+
+        public Ingenieur(string nom, string prenom, string specialite)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.specialite = specialite;
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return this.nom;
+            }
+            set
+            {
+                this.nom = value;
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return this.prenom;
+            }
+
+            set
+            {
+                this.prenom = value;
+            }
+        }
+
+        public string Specialite
+        {
+            get
+            {
+                return this.specialite;
+            }
+            set
+            {
+                this.specialite = value;
+            }
+        }
+
+    }
+
+    class Technicien
+    {
+        private string nom;
+        private string prenom;
+
+        public Technicien(string nom, string prenom)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return this.nom;
+            }
+            set
+            {
+                this.nom = value;
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return this.prenom;
+            }
+            set
+            {
+                this.prenom = value;
+            }
+        }
+    }
+}
---
+++ 4. Requêtes et variables/4-variables-exercices-solutions.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqFoxxpy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Personne> personnes = new List<Personne>
+            {
+                new Personne("Beauvoir", "Simon", 16, "M"),
+                new Personne("Beauvoir", "Simone", 25, "F"),
+                new Personne("De Caunes", "Richard", 41, "M"),
+                new Personne("Sullivan", "Sullivan", 31, "M"),
+                new Personne("Rémy", "Camille", 22, "F"),
+                new Personne("Manchon", "Camille", 19, "M"),
+                new Personne("Thiebaud", "Marie", 61, "F"),
+                new Personne("Crouchon", "Mélanie", 55, "F"),
+                new Personne("Baline", "Mélodie", 74, "F"),
+                new Personne("Karine", "Pascal", 31, "M"),
+                new Personne("Katherine", "Pascale", 36, "F"),
+                new Personne("Zoula", "Charles", 20, "M"),
+                new Personne("Romain", "Collin", 34, "M"),
+                new Personne("Fouchard", "Aïcha", 48, "F"),
+                new Personne("Blandine", "Maëva", 18, "F")
+            };
+
+            //Créer une variable majeur qui est égale à True si l'âge de la personne est supérieure ou égale à 18
+            //sinon False
+            var query = from p in personnes
+                        let nom_complet = p.Nom + " " + p.Prenom
+                        select new {nom_complet = nom_complet, Age = p.Age };
+
+
+            //Créer une variable "initiale" qui contient seulement les initiales du nom et du prénom : p.Nom[0]+"."+p.Prenom[0]
+            //Sélectionner seulement les personnes majeures (18 ans et plus)
+            //Sélectionner également les personnes âgées de moins de 50 ans
+            //Créer une variable taille_nom_complet = longueur du prénom + longueur du nom
+            //Créer un objet anonyme avec les attributs : Nom, prénom, initiale, taille_nom_complet, age
+
+            var bigQuery = from p in personnes
+                           let initiale = p.Nom[0] + "." + p.Prenom[0]
+                           let taille_nom_complet = p.Nom.Length + p.Prenom.Length
+                           where p.Age >= 18 && p.Age < 50
+                           select new {Nom = p.Nom, Prenom = p.Prenom, Initiale = initiale, Taille_nom_complet = taille_nom_complet, Age = p.Age  };
+
+            //Affichage du résultat de la requête
+            foreach (var item in bigQuery)
+            {
+                Console.WriteLine(item.Nom+" "+item.Prenom+" "+item.Initiale+" "+item.Taille_nom_complet+" "+item.Age);
+            }
+
+        }
+
+    }
+
+    class Personne
+    {
+        private string nom;
+        private string prenom;
+        private int age;
+        private string sexe;
+        private bool est_ingenieur;
+
+        public Personne(string nom, string prenom, bool ingenieur)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.est_ingenieur = ingenieur;
+        }
+
+        public Personne(string nom, string prenom, int age, string sexe)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.age = age;
+            this.sexe = sexe;
+        }
+
+        public Personne(string nom, string prenom, int age, string sexe, bool ingenieur)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.age = age;
+            this.sexe = sexe;
+            this.est_ingenieur = ingenieur;
+        }
+
+        public string Nom
+        {
+            get
+            {
+                return this.nom;
+            }
+            set
+            {
+                this.nom = value;
+            }
+        }
+
+        public string Prenom
+        {
+            get
+            {
+                return this.prenom;
+            }
+
+            set
+            {
+                this.prenom = value;
+            }
+        }
+
+        public bool Est_ingenieur
+        {
+            get
+            {
+                return this.est_ingenieur;
+            }
+            set
+            {
+                this.est_ingenieur = value;
+            }
+        }
+
+        public string Sexe
+        {
+            get
+            {
+                return this.sexe;
+            }
+            set
+            {
+                this.sexe = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            set
+            {
+                this.age = value;
+            }
+        }
+
+    }
+
+}
